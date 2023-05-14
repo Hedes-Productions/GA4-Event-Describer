62fda391cfab24f22f5aaea01b1d0cc020d641e8
Hedes-Productions <tharindugimras@gmail.com>
2023-05-14 15:12:42 +0530

    Modify the authentication method for OAuth2

--- src/auths/oauth2.js
+++ src/auths/oauth2.js
@@ -1,54 +1,35 @@
-const http = require('http');
-const destroyer = require('server-destroy');
-const url = require('url');
-const { google } = require('googleapis');
+const { google } = require("googleapis");
 const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../../.env") });
 
-exports.authenticate = async (scopes) => {
-  const { default: open } = await import('open');
-  const oauth2Client = new google.auth.OAuth2(
-    process.env.CLIENT_ID,
-    process.env.CLIENT_SECRET,
-    process.env.REDIRECT_URI
-  );
+const oauth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URI
+);
 
+exports.authURLGenerator = async (scopes) => {
+  const { default: open } = await import("open");
   const authorizeUrl = oauth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: scopes.join(' '),
+    access_type: "offline",
+    scope: scopes,
   });
+  open(authorizeUrl, { wait: false }).then((cp) => cp.unref());
+};
 
-  return new Promise((resolve, reject) => {
-    const requestHandler = async (req, res) => {
-      try {
-        if (req.url.indexOf('/oauth2callback') > -1) {
-          const qs = new url.URL(req.url, 'http://localhost:3000').searchParams;
-          const { tokens } = await oauth2Client.getToken(qs.get('code'));
-          console.log(tokens)
-          oauth2Client.setCredentials(tokens);
-          res.end('Authentication successful! Please return to the console.');
-          serverInstance.destroy();
-          resolve(oauth2Client);
-        }
-      } catch (e) {
-        console.log(e);
-        reject(e);
-      }
-    };
+exports.getToken = async (code) => {
+  const { tokens } = await oauth2Client.getToken(code);
+  return tokens;
+};
 
-    const serverInstance = http.createServer((req, res) => {
-      res.writeHead(200);
-      res.end('OK');
-    });
+exports.setAccessToken = (tokens) => {
+  oauth2Client.setCredentials(tokens);
+};
 
-    serverInstance.listen(3000, () => {
-      // open the browser to the authorize url to start the workflow
-      open(authorizeUrl, { wait: false }).then((cp) => cp.unref());
-    });
-
-    destroyer(serverInstance);
-
-    serverInstance.on('request', requestHandler);
-    serverInstance.on('error', reject);
-  });
+exports.handleAccessToken = () => {
+  const credentials = oauth2Client.credentials;
+  if (credentials) {
+    console.log(credentials.refresh_token);
+  }
+  console.log(credentials.access_token);
 };
--- src/server.js
+++ src/server.js
@@ -1,19 +1,32 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const { authenticate } = require('./auths/oauth2');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const {
+  authURLGenerator,
+  getToken,
+  handleAccessToken,
+  setAccessToken,
+} = require("./auths/oauth2");
 
 const app = express();
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
-const scopes = [
-    'https://www.googleapis.com/auth/analytics.readonly'
-  ];
+const scopes = ["https://www.googleapis.com/auth/analytics.readonly"];
 
-authenticate(scopes);
+app.listen(PORT, () => {
+  authURLGenerator(scopes);
+  console.log(`Server running at http://localhost:${PORT}`);
+});
+
+app.get("/oauth2callback", async (req, res) => {
+  const code = req.query.code;
+  const token = await getToken(code);
+  setAccessToken(token);
+  handleAccessToken();
+  res.end("Authentication successful! Please return to the console.");
+});
