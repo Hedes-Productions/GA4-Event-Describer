b06ec25492f3148ddd9e6ce72bce58b112df6629
Hedes-Productions <tharindugimras@gmail.com>
2023-05-28 10:28:27 +0530

    Remove console log and format code

--- backend/src/clients/redis.client.js
+++ backend/src/clients/redis.client.js
@@ -2,12 +2,12 @@ const redis = require("redis");
 const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../../.env") });
 
-if (process.env.DOCKER_CONTAINER === 'true') {
-  host = 'host.docker.internal';
+if (process.env.DOCKER_CONTAINER === "true") {
+  host = "host.docker.internal";
 } else {
-  host = 'localhost';
+  host = "localhost";
 }
-console.log(process.env.DOCKER_CONTAINER)
+
 exports.redisClient = redis.createClient({
   socket: {
     host: host,
