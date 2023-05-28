445610f488d6352bd9233e0f5860d6d940e797ba
Hedes-Productions <tharindugimras@gmail.com>
2023-05-28 10:25:41 +0530

    Move files to backend

--- backend/src/clients/redis.client.js
+++ backend/src/clients/redis.client.js
@@ -2,9 +2,15 @@ const redis = require("redis");
 const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "../../.env") });
 
+if (process.env.DOCKER_CONTAINER === 'true') {
+  host = 'host.docker.internal';
+} else {
+  host = 'localhost';
+}
+console.log(process.env.DOCKER_CONTAINER)
 exports.redisClient = redis.createClient({
   socket: {
-    host: "host.docker.internal",
+    host: host,
     port: 6379,
   },
 });
